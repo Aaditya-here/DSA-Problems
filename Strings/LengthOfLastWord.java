e55390cb2ee062070da03ac90151a411ae08b5f0
Aaditya <aadityapatilpush@gmail.com>
2026-03-16 22:11:14 +0530

    2026-03-16 | Added Length of last word

---
+++ Strings/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+package Interview.Strings;
+
+public class LengthOfLastWord {
+
+	public int lengthOfLastWord(String s) {
+
+		int length = 0;
+		int i = s.length() - 1;
+
+		while (i >= 0 && s.charAt(i) == ' ') {
+			i--;
+		}
+
+		while (i >= 0 && s.charAt(i) != ' ') {
+
+			length++;
+			i--;
+		}
+
+		return length;
+	}
+
+	public static void main(String[] args) {
+		String s = "Hello world in java";
+		LengthOfLastWord lastWord = new LengthOfLastWord();
+		int result = lastWord.lengthOfLastWord(s);
+		System.out.println(result);
+	}
+}

1ed4235c4c9c567ab72795bc09aed416166a023b
Aaditya <aadityapatilpush@gmail.com>
2026-03-27 19:39:49 +0530

    2026-03-27 | Added math/SquareRoot

---
+++ math/SquareRoot.java
@@ -0,0 +1,43 @@
+package Interview.math;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+
+	public int mySqrt(int x) {
+
+		if (x == 0 || x == 1) {
+			return x;
+		}
+		int left = 1;
+		int right = x;
+		int ans = 0;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (mid <= x / mid) {
+
+				ans = mid;
+				left = mid + 1;
+
+			} else {
+				right = mid - 1;
+			}
+
+		}
+
+		return ans;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		SquareRoot root = new SquareRoot();
+		int result = root.mySqrt(x);
+		System.out.println(result);
+
+	}
+
+}
